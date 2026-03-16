7696a83f482ad1cf68cd4713ec92bd3927c48f2a
Manish  Mishra <manishweb9199@gmail.com>
2026-03-16 23:41:39 +0530

    Added Java basics program

---
+++ A.java
@@ -0,0 +1,8 @@
+class A
+{
+   public static void main(String[]args)
+{
+  System.print.ln("hello manish");
+}
+
+}
---
+++ B.java
@@ -0,0 +1,8 @@
+class B
+{
+   public static void main(String[]args)
+{
+  System.print.ln("hello Ram");
+}
+
+}
