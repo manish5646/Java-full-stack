12041a1dc1cef7338823f8f13ce0f40e3f3864b9
Manish  Mishra <manishweb9199@gmail.com>
2026-03-24 00:13:47 +0530

    Add files via upload
    using operators in java

---
+++ Even&Odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Even&odd
+{
+public static void main(String[]args)
+{
+Scanner scn=new Scanner(System.in);
+System.out.println("enter a num");
+ int n=scn.nextInt();
+if (n%2==0){
+   System.out.println("even num");
+}
+else
+  System.out.println("odd num");
+
+   }
+}
