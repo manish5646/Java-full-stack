9cb6afa7be895da35d09ded97dd10969a7323947
Manish  Mishra <manishweb9199@gmail.com>
2026-03-24 23:55:11 +0530

    Add files via upload

---
+++ MonthDay.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class MonthDay
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+
+        int m = scn.nextInt();
+        int y = scn.nextInt();
+
+        switch (m)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 days");
+                break;
+
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 days");
+                break;
+
+            case 2:
+                if (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0))
+                {
+                    System.out.println("29 days");
+                }
+                else
+                {
+                    System.out.println("28 days");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid input");
+        }
+    }
+}
