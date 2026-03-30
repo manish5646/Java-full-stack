2bb24574c5698c5548b9d9b163064605978675fc
Manish  Mishra <manishweb9199@gmail.com>
2026-03-30 19:08:44 +0530

    Add Star1.java to print square star pattern
    Implement a program to print a square pattern of stars.

---
+++ Pattern-Problem/Star1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Star1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
