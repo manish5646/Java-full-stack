0bd6b01ec6417c3f649d03dd7dbf6ed6d40292bf
Manish  Mishra <manishweb9199@gmail.com>
2026-03-31 22:44:07 +0530

    Add Pyramid pattern printing program

---
+++ Pattern-Problem/Pyramid.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Pyramid {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int sp = n - 1;
+        int st = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            
+            for (int j = 1; j <= sp; j++) {
+                System.out.print(" ");
+            }
+
+                        for (int k = 1; k <= st; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            sp--;
+            st += 2;
+        }
+    }
+}
