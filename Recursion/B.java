bfcd4ff53eda20428ae2fcb1389dcb42580ce0eb
Manish  Mishra <manishweb9199@gmail.com>
2026-04-08 23:16:41 +0530

    Create B.java

---
+++ Recursion/B.java
@@ -0,0 +1,14 @@
+class B
+{
+public static void main(String[]args)
+{
+  m1(1);
+}
+static void m1(int i){
+System.out.println(i);
+if (i>=3) return;
+ m1(i+1);
+ m1(i+1);
+System.out.println(i);
+}
+}
